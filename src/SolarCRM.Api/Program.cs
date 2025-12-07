d091a72e7c72342d17f5ab68d724e692e67ead63
IcaroTimoteo96 <timoteoicaro18@gmail.com>
2025-12-07 13:03:23 -0300

    feat: config para deploy

--- src/SolarCRM.Api/Program.cs
+++ src/SolarCRM.Api/Program.cs
@@ -33,7 +33,8 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowReactApp", policy =>
     {
-        policy.WithOrigins("http://localhost:5173", "http://localhost:3000")
+        var allowedOrigins = builder.Configuration.GetSection("AllowedOrigins").Get<string[]>() ?? [];
+        policy.WithOrigins(allowedOrigins)
               .AllowAnyHeader()
               .AllowAnyMethod();
     });
